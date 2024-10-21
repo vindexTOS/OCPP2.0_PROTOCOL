560f2a7f5e4290faa2e1e0e5cfeb97d443c9831c
vindexTOS <giorgikutateladze1998@gmail.com>
2024-10-21 14:20:37 +0400

    change port to 4002

--- frontend/ocpp-csms-angular/server.ts
+++ frontend/ocpp-csms-angular/server.ts
@@ -49,7 +49,7 @@ function run(): void {
   // Start up the Node server
   const server = app();
   server.listen(port, () => {
-    console.log(`Node Express server listening on http://37.27.179.61/${port}`);
+    console.log(`Node Express server listening on http://37.27.179.61:${port}`);
   });
 }
 
